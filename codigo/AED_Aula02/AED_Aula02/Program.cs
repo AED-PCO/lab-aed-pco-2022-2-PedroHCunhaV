9abc32983d1151a754ebbbcdb00bc29577c467ac
Pedro Henrique Cunha Vieira <1396015@sga.pucminas.br>
2022-09-08 20:08:28 -0300

    Atualizações dos Códigos Referentes ao Segundo Laboratório

--- codigo/AED_Aula02/AED_Aula02/Program.cs
+++ codigo/AED_Aula02/AED_Aula02/Program.cs
@@ -27,6 +27,9 @@ do
         case 6:
             Atividade6();
             break;
+        case 7:
+            Atividade7();
+            break;
         case 0:
             break;
     }
@@ -177,8 +180,42 @@ do
     }
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------
-
+    
     static void Atividade6()
+    {
+        Console.WriteLine("\nRetirar as Vogais de uma Frase ou palavra mostrando somente suas consoantes");
+
+        string frase;
+        Console.WriteLine("\nDigite a frase desejada: ");
+        RetiraVogal(Console.ReadLine());
+    }
+
+    static string RetiraVogal(string frase)
+    {
+        if(frase.Length == 0)
+        {
+            return "";
+        }
+        
+        char[] cont = frase.ToCharArray();
+
+        if (cont[cont.Length-1]=='a'|| cont[cont.Length - 1] == 'e' || cont[cont.Length - 1] == 'i' || cont[cont.Length - 1] == 'o'|| cont[cont.Length - 1] == 'u')
+        {
+            Array.Resize(ref cont, cont.Length-1);
+        }
+        else
+        {
+            Console.WriteLine(cont[cont.Length-1]);
+            Array.Resize(ref cont, cont.Length-1);
+        }
+        
+        frase = new string(cont);
+        return RetiraVogal(frase);
+    }
+
+    //-------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    static void Atividade7()
     {
         Console.WriteLine("Definição recursiva da série de Fibonacci");
 
